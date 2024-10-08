d0e936387a658e69a73f339fdd9432aede1ac2b5
Jack <jackmadden13@hotmail.co.uk>
2024-10-08 18:24:47 +0100

    Saved seed data in db and lowercased keys in countries.js

---
+++ seeds/index.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Holidays = require('../models/holidays');
+const countries = require('./countries');
+
+mongoose.connect('mongodb://localhost:27017/holidays');
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error'));
+db.once('open', () => {
+    console.log('Database connected');
+});
+
+const seedDb = async() => {
+    await Holidays.deleteMany({});
+    for (let i = 0; i < 25; i++){
+        const country = new Holidays({
+            destination: `${countries[i].destination}, ${countries[i].traveler_name}`
+        })
+        await country.save();
+        }
+    }
+
+seedDb();
